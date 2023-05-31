4167226d3c6b48e7b66bc1c6eddd163f50272443
HJAC08 <107573356+HJAC08@users.noreply.github.com>
2023-05-31 16:51:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+noseX=0;
+noseY=0;
 function preload() {
 
 }
@@ -19,6 +21,9 @@ function modelLoaded() {
 
 function draw() {
  image(video, 0, 0, 300, 300);
+ fill(255, 0, 0);
+ stroke(255, 0, 0,);
+ circle(noseX, noseY, 20);
 }
 function take_snapshot(){
     save('myFilterImage.png')
@@ -29,7 +34,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
